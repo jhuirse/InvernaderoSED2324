f11ad27f8e4285e25f5d71affc7168348a58a37f
jhuirse <80428998+jhuirse@users.noreply.github.com>
2024-01-21 19:48:19 +0100

    Funciones Set_Pin_Input y Set_Pin_Output

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -56,6 +56,26 @@ static void MX_GPIO_Init(void);
 /* USER CODE BEGIN 0 */
 uint8_t Hum_Int,Hum_Dec,Temp_Int,Temp_Dec,Sum;
 float Temperatura, Humedad;
+
+void Set_Pin_Output(GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin)
+{
+	GPIO_InitTypeDef GPIO_InitStruct ={0};
+	GPIO_InitStruct.Pin = GPIO_Pin;
+	GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+	GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+	HAL_GPIO_Init(GPIOx, &GPIO_InitStruct);
+}
+
+void Set_Pin_Input(GPIO_TypeDef *GPIOx, uint16_t GPIO_Pin)
+{
+	GPIO_InitTypeDef GPIO_InitStruct ={0};
+	GPIO_InitStruct.Pin = GPIO_Pin;
+	GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+	GPIO_InitStruct.Pull = GPIO_NOPULL;
+	HAL_GPIO_Init(GPIOx, &GPIO_InitStruct);
+}
+
+
 void DHT11_Start (void)
 {
 	Set_Pin_Output (GPIOA, DHT11);
